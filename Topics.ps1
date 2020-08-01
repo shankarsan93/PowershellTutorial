d38966c5ba598724f55c6a3bea2f46617758bd9c
shankar <admin@home.com>
2020-08-01 21:55:34 +0530

    Initial commit

---
+++ Topics.ps1
@@ -0,0 +1,19 @@
+﻿<#
+Training topics:
+----------------
+    -> Objects
+    -> Data Types
+    -> Variables
+    -> Cmdlets
+    -> Structure of a Cmdlet
+    -> Parameters and Arguments
+    -> Comparison Operators
+    -> Conditional Statements / Flow Control Statements
+    -> Logical Operators
+    -> Looping Statements
+    -> Object Piping
+    -> Filtering Statements
+    -> Error Handling
+    -> Functions
+    -> Working with Modules
+#>
